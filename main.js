cf5410017b053534a65330eb0418cc8d1f8cc33f
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2025-07-18 11:50:36 -0500

    HG task 0 testing main.js and utils/redis.js

---
+++ main.js
@@ -0,0 +1,14 @@
+import redisClient from './utils/redis.js';
+
+(async () => {
+  console.log('Is Redis alive?', redisClient.isAlive()); // Should print true
+
+  console.log('Initial get for myKey:', await redisClient.get('myKey')); // Should print null
+
+  await redisClient.set('myKey', 12, 5); // Set key with value 12, expires in 5 seconds
+  console.log('After set, get myKey:', await redisClient.get('myKey')); // Should print '12'
+
+  setTimeout(async () => {
+    console.log('After 10s, get myKey again:', await redisClient.get('myKey')); // Should print null
+  }, 10000);
+})();
