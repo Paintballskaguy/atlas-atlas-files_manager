f2933b006972f4134d393ba0b840a382fc4191a1
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2025-07-22 02:25:30 -0500

    had to change to port 5001 bc of unknown error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes/index.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
